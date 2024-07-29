70b2a94046c87185026d7d1fc1b1eac22bc9e84b
zenonet <78077158+zenonet@users.noreply.github.com>
2024-07-29 12:09:07 +0200

    The parser is now able to find top level properties in a JSON string

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <cstring>
 
+struct JsonValue{
+    char* rawJson;
+    int startIndex;
+    int endIndex;
+
+    char* asString() const{
+        char* str = static_cast<char *>(malloc(endIndex - startIndex));
+        memcpy(str, rawJson+startIndex, endIndex-startIndex);
+        return str;
+    }
+};
 
 struct JsonElement{
     char* rawJson;
@@ -11,10 +22,25 @@ struct JsonElement{
         std::cout << startIndex;
     }
 
-    char* getString(char* name){
-        for(int i=0; i<=endIndex;i++){
+    JsonValue getValue(char* name){
+        for(int i=startIndex; i<=endIndex;i++){
             skipWhitespace(rawJson, i, endIndex);
+
+            int nameStartIndex = i+1;
+            int nameLength = getStringLiteralLengthAndSkip(rawJson, i, endIndex);
+            if(strcmp(rawJson+nameStartIndex, name, nameLength)){
+                std::cout << "Found property: " << name << "\n";
+                break;
+            }
+            skipWhitespace(rawJson, i, endIndex);
+            if(rawJson[i++] != ':') throw std::invalid_argument("Expected colon after property name in JSON");
+            skipWhitespace(rawJson, i, endIndex);
+
+            skipJsonValue(rawJson, i, endIndex);
+
+            if(rawJson[i] != ',' && rawJson[i] != '}') throw std::invalid_argument("Expecting comma after property in JSON");
         }
+        return JsonValue();
     }
 
     JsonElement(char* rawJson){
@@ -35,14 +61,43 @@ private:
         endIndex = end;
     }
     void skipWhitespace(char* source, int& i, int endIndex){
-        while(i<endIndex && iswspace(source[i])){
-            i++;
+        for(;i<=endIndex && iswspace(source[i]);i++){}
+    }
+
+    int getStringLiteralLengthAndSkip(char* source, int& i, int endIndex){
+        if(source[i++] != '\"') throw std::invalid_argument("String literal does not start with quotation marks.");
+        int len = 0;
+        for(;i<=endIndex && source[i] != '\"';i++,len++){
+            // Allow escaping quotation marks
+            if(source[i] == '\\' && i < endIndex) i++;
         }
+        if(source[i++] != '\"') throw std::invalid_argument("String literal does not end with quotation marks.");
+        return len;
+    }
+
+    void skipJsonValue(char* source, int& i, int endIndex){
+        if(source[i] == '{' || source[i] == '['){
+            // Analyze brace pattern
+
+        }else{
+            // Skip to the next comma
+            for(;i<=endIndex && source[i] != ',';i++){}
+        }
+
+    }
+
+    static bool strcmp(char* a, char* b, int length){
+        for(int i=0; i<length; i++){
+            if(a[i] != b[i]) return false;
+        }
+        return true;
     }
 };
 
+
 int main() {
-    char* rawJson = (char*)"{\"yeet\":16}";
+    char* rawJson = (char*)"{\"yeet\":16, \"yote\":25}";
     JsonElement element(rawJson);
+    JsonValue val = element.getValue("yote");
     return 0;
 }
